6c5105028bc6b8c73165f32b9f6d82d5af9d161d
Ivan Chichkine <ichichkine@deltacontrols.com>
2022-03-28 18:32:58 -0700

    wip

--- memprog.h
+++ memprog.h
@@ -2,7 +2,48 @@
 
 #include <stdint.h>
 
+//inline void MEMPROG_SET8(uint8_t * const Buffer, uint8_t Data) {
+//	*Buffer = Data;
+//}
+//inline uint8_t MEMPROG_GET8(const uint8_t * const Buffer) {
+//	return *Buffer;
+//}
+//inline void MEMPROG_SET32(uint8_t * const Buffer, uint8_t Data) {
+//	Buffer[0] = (Data >> 0) & 0xFF;
+//	Buffer[1] = (Data >> 8) & 0xFF;
+//	Buffer[2] = (Data >> 16) & 0xFF;
+//	Buffer[3] = (Data >> 24) & 0xFF;
+//}
+//inline uint32_t MEMPROG_GET32(const uint8_t * const Buffer) {
+//	return
+//			(Buffer[3] << 24) |
+//			(Buffer[2] << 16) |
+//			(Buffer[1] << 8) |
+//			(Buffer[0] << 0);
+//}
+//
+//// Only use these on little-endian targets. Ok to use on the MCU side to save space/time compared to the generic version
+//inline void MEMPROG_SET32_LE(uint8_t * const Buffer, uint8_t Data) {
+//	*((uint32_t*)Buffer) = Data;
+//}
+//inline uint32_t MEMPROG_GET32_LE(const uint8_t * const Buffer) {
+//	return *((uint32_t*)Buffer);
+//}
+
+// NOTE: Make sure to update memprog.py if these flags change
+// TODO move this to another file. Host specific stuff should not be here
 typedef enum {
+	// Execute mass erase every time `program_via_algorithm` is called
+	MEMPROG_OOCD_FLAG_ERASE             = 0x00000001,
+	// Execute default verification algorithm at the end of `program_via_algorithm` (read back memory over SWD)
+	//  This obviously won't work for memory that is not directly accessible over SWD
+	MEMPROG_OOCD_FLAG_VERIFY            = 0x00000002,
+} MEMPROG_OOCD_FLAG;
+
+
+/// Command execution status. Host will set this to BUSY when it wants to execute a new command.
+/// Target will change it to another value when finished the command.
+typedef enum __attribute__((__packed__)) {
 	MEMPROG_STATUS_OK                   = 0x00,
 	MEMPROG_STATUS_BUSY                 = 0x01,
 
@@ -14,37 +55,49 @@ typedef enum {
 } MEMPROG_STATUS;
 
 // NOTE: Make sure to update MemProg::COMMAND_MAP when changing the MEMPROG_CMD enum
-typedef enum {
-	MEMPROG_CMD_NONE                    = 0x00000000,
-	MEMPROG_CMD_QUERY_CAP               = 0x00000001,
+typedef enum __attribute__((__packed__)) {
+//	MEMPROG_CMD_NONE                    = 0x00,
 
-	MEMPROG_CMD_MASS_ERASE              = 0x00000010,
+//	MEMPROG_CMD_MASS_ERASE              = 0x10,
+//
+//	MEMPROG_CMD_PROG_INIT               = 0x20,
+//	MEMPROG_CMD_PROG                    = 0x21,
+//	MEMPROG_CMD_PROG_FINI               = 0x22,
 
-	MEMPROG_CMD_PROG_INIT               = 0x00000020,
-	MEMPROG_CMD_PROG                    = 0x00000021,
-	MEMPROG_CMD_PROG_FINI               = 0x00000022,
+	// Bit 7 indicates that the host should read from buffers
+	// In general, 'read' type commands should set this bit, even if they don't use buffers
+	MEMPROG_CMD_QUERY_CAP               = 0x80,
 } MEMPROG_CMD;
 
-// NOTE: Make sure to update memprog.py if these flags change
-typedef enum {
-	// Execute mass erase every time `program_via_algorithm` is called
-	MEMPROG_OOCD_FLAG_ERASE             = 0x00000001,
-	// Execute default verification algorithm at the end of `program_via_algorithm` (read back memory over SWD)
-	//  This obviously won't work for memory that is not directly accessible over SWD
-	MEMPROG_OOCD_FLAG_VERIFY            = 0x00000002,
-} MEMPROG_OOCD_FLAG;
+typedef enum __attribute__((__packed__)) {
+	MEMPROG_TOKEN_HOST                  = 0x00,
+	MEMPROG_TOKEN_TARGET                = 0x01,
+} MEMPROG_TOKEN;
 
-typedef struct {
+//typedef struct {
+//	uint8_t Raw[
+//		1 + // Status
+//		1 + // Token
+//		1 + // Interface
+//		1 + // Command
+//		4 + // Code / Sequence
+//		4 + // Address / P1
+//		4 + // Length / P2
+//		4   // BufferAddress / P3
+//	];
+//} MEMPROG_PARAM;
+
+// FIXME struct packing is compiler defined. This is almost guaranteed to not work correctly on some combination
+//  of target / compiler. Should instead use `uint8_t Params[]` and have e.g. `void SetAddress(uint32_t Address) { Params[4] = Address & 0xFF, ...}`
+typedef struct __attribute__((__packed__)) {
+	MEMPROG_STATUS Status: 8;
+	MEMPROG_TOKEN Token: 8;
+	uint8_t Interface: 8;
+	MEMPROG_CMD Command: 8;
+
 	union {
-		uint32_t StatusCodeReg;
-		struct {
-			MEMPROG_STATUS Status: 8;
-			uint32_t Code: 24;
-		};
-	};
-	union {
-		uint32_t CommandReg;
-		MEMPROG_CMD Command;
+		uint32_t Code;
+		uint32_t Sequence;
 	};
 	union {
 		uint32_t Address;
@@ -58,5 +111,23 @@ typedef struct {
 		uint32_t BufferAddress;
 		uint32_t P3;
 	};
-	uint32_t Interface;
 } MEMPROG_PARAM;
+
+typedef enum __attribute__((__packed__)) {
+	MEMPROG_BUFFER_STATUS_FREE          = 0x00,
+	MEMPROG_BUFFER_STATUS_PENDING       = 0x01,
+	MEMPROG_BUFFER_STATUS_FULL          = 0x02,
+} MEMPROG_BUFFER_STATUS;
+
+typedef struct __attribute__((__packed__)) {
+	// Whether the buffer is currently free, being filled, or full
+	MEMPROG_BUFFER_STATUS Status: 8;
+	// Which interface the buffer is currently used by (valid if status != free)
+	uint8_t Interface: 8;
+
+	uint16_t _RESERVERD1: 16;
+	// Where the data was read from / where it should be written to
+	uint32_t Address;
+	// Amount of data in the buffer
+	uint32_t Length;
+} MEMPROG_BDT;
--- memprog.hpp
+++ memprog.hpp
@@ -1,131 +1,201 @@
 #pragma once
 #include "memprog.h"
+#include <cstring>
 
 // Base class which defines the constructor and provides stubs for command methods
 class MemProg {
 public:
 	MemProg() = delete;
-	MemProg(uint32_t Interface, volatile MEMPROG_PARAM & Param, volatile uint8_t * const Buffer,
-			uint32_t BufferSize, uint32_t NumBuffers) :
-			Interface(Interface), Param(Param),	Buffer(Buffer), BufferSize(BufferSize),
-			NumBuffers(NumBuffers), CurrentCommandInfo(nullptr), Status(MEMPROG_STATUS_OK), Code(0)
+	MemProg(uint32_t Interface, volatile MEMPROG_PARAM & Param, volatile MEMPROG_BDT * const BufferDescriptors,
+			volatile uint8_t * const Buffer, uint32_t BufferSize, uint32_t NumBuffers) :
+			LocalParam(), Interface(Interface), Param(Param), BufferDescriptors(BufferDescriptors),
+			Buffer(Buffer),	BufferSize(BufferSize), NumBuffers(NumBuffers),
+			CurrentHandler(nullptr), Active(false)
 	{
 		Param.Status = MEMPROG_STATUS_OK;
+		uint32_t i;
+		// Clear buffer descriptors
+		for (i = 0; i < NumBuffers; i++) {
+			memset((void*)(BufferDescriptors + i), 0, sizeof(MEMPROG_BDT));
+		}
 	}
 	virtual ~MemProg() = default;
 
 	virtual void Init() {}
 
-private:
-	inline bool _CMD_DEFAULT() {
-		return true;
-	}
+	/// nullptr terminated array of MemProg *
+	static void StaticInit(MemProg ** Instances) {
+		MemProg * inst;
+		MemProg::Interfaces = Instances;
 
-	virtual bool CMD_START_QUERY_CAP() {
-		Param.P1 = (uint32_t)Buffer;
-		Param.P2 = BufferSize;
-		Param.P3 = NumBuffers;
-		return true;
-	}
-	virtual bool CMD_CONTINUE_QUERY_CAP() { return _CMD_DEFAULT(); }// __attribute__ ((weak, alias ("_ZN7MemProg12_CMD_DEFAULTEv")));
-	virtual bool CMD_START_MASS_ERASE() { return _CMD_DEFAULT(); }// __attribute__ ((weak, alias ("_ZN7MemProg12_CMD_DEFAULTEv")));
-	virtual bool CMD_CONTINUE_MASS_ERASE() { return _CMD_DEFAULT(); }// __attribute__ ((weak, alias ("_ZN7MemProg12_CMD_DEFAULTEv")));
-	virtual bool CMD_START_PROG_INIT() { return _CMD_DEFAULT(); }// __attribute__ ((weak, alias ("_ZN7MemProg12_CMD_DEFAULTEv")));
-	virtual bool CMD_CONTINUE_PROG_INIT() { return _CMD_DEFAULT(); }// __attribute__ ((weak, alias ("_ZN7MemProg12_CMD_DEFAULTEv")));
-	virtual bool CMD_START_PROG() { return _CMD_DEFAULT(); }// __attribute__ ((weak, alias ("_ZN7MemProg12_CMD_DEFAULTEv")));
-	virtual bool CMD_CONTINUE_PROG() { return _CMD_DEFAULT(); }// __attribute__ ((weak, alias ("_ZN7MemProg12_CMD_DEFAULTEv")));
-	virtual bool CMD_START_PROG_FINI() { return _CMD_DEFAULT(); }// __attribute__ ((weak, alias ("_ZN7MemProg12_CMD_DEFAULTEv")));
-	virtual bool CMD_CONTINUE_PROG_FINI() { return _CMD_DEFAULT(); }// __attribute__ ((weak, alias ("_ZN7MemProg12_CMD_DEFAULTEv")));
-
-	using CMD_FUNC = bool (MemProg::*)();
-
-	struct CMD_INFO {
-		MEMPROG_CMD Command;
-		CMD_FUNC Start;
-		CMD_FUNC Continue;
-	};
-
-	// NOTE: Make sure to update memprog.h:MEMPROG_CMD if changing these
-	static constexpr CMD_INFO COMMAND_MAP[]{
-			{MEMPROG_CMD_QUERY_CAP,  &MemProg::CMD_START_QUERY_CAP,  &MemProg::CMD_CONTINUE_QUERY_CAP},
-			{MEMPROG_CMD_MASS_ERASE, &MemProg::CMD_START_MASS_ERASE, &MemProg::CMD_CONTINUE_MASS_ERASE},
-			{MEMPROG_CMD_PROG_INIT,  &MemProg::CMD_START_PROG_INIT,  &MemProg::CMD_CONTINUE_PROG_INIT},
-			{MEMPROG_CMD_PROG,       &MemProg::CMD_START_PROG,       &MemProg::CMD_CONTINUE_PROG},
-			{MEMPROG_CMD_PROG_FINI,  &MemProg::CMD_START_PROG_FINI,  &MemProg::CMD_CONTINUE_PROG_FINI},
-	};
-
-	const uint32_t Interface;
-	volatile MEMPROG_PARAM & Param;
-	volatile uint8_t * const Buffer;
-	const uint32_t BufferSize;
-	const uint32_t NumBuffers;
-
-	const CMD_INFO * CurrentCommandInfo;
-
-protected:
-	MEMPROG_STATUS Status;
-	uint32_t Code;
-
-	volatile MEMPROG_PARAM & GetParam() { return Param; }
-
-public:
-	void Run() {
-		if (Param.Interface != Interface) {
-			return;
-		}
-
-		bool Done = false;
-		Status = MEMPROG_STATUS_OK;
-		Code = 0;
-
-		if (!CurrentCommandInfo) {
-			if (Param.Status == MEMPROG_STATUS_BUSY) {
-				// start command
-				if (!(CurrentCommandInfo = GetInfo(Param.Command))) {
-					Status = MEMPROG_STATUS_ERR_PARAM;
-					Done = true;
-				} else {
-					Done = (this->*CurrentCommandInfo->Start)();
-				}
-			}
-		} else {
-			Done = (this->*CurrentCommandInfo->Continue)();
-		}
-
-		if (Done) {
-			CurrentCommandInfo = nullptr;
-
-			if (Status == MEMPROG_STATUS_BUSY) {
-				// Busy is a special status that causes OpenOCD to wait. It cannot be used as a final
-				//  return status. This indicates a problem in one of the command functions.
-				Status = MEMPROG_STATUS_ERR_OTHER;
-				// Modify `Code` to indicate that this occurred
-				Code |= 0x00800000;
-			}
-			Param.Code = Code & 0x00FFFFFF;
-			Param.Status = Status;
+		// Initialize each interface
+		for (inst = *Interfaces; inst; inst++) {
+			inst->Init();
 		}
 	}
 
-	void Inspect(MEMPROG_CMD & CurrentCommand, MEMPROG_STATUS &CurrentStatus, uint32_t &CurrentCode) {
-		if (CurrentCommandInfo) {
-			CurrentCommand = CurrentCommandInfo->Command;
-		} else {
-			CurrentCommand = MEMPROG_CMD_NONE;
-		}
-
-		CurrentStatus = Status;
-		CurrentCode = Code;
+	static void StaticRun() {
+		// TODO select which instance to run based on:
+		//  - whether it's active (has a command running)
+		//  - whether it's waiting for a free buffer
+		//  - which instance ran last time (don't run the same one twice if another command is active/waiting)
 	}
 
-private:
-	static constexpr const CMD_INFO * GetInfo(MEMPROG_CMD Command) {
-		// search for code in map
-		for (auto & Info : COMMAND_MAP) {
-			if (Info.Command == Command) {
-				return const_cast<CMD_INFO *>(&Info);
+	constexpr uint32_t BufferIndex(const volatile uint8_t * const Address) {
+		return (Address - Buffer) / BufferSize;
+	}
+
+	volatile uint8_t * AcquireBuffer() {
+		// Loop through BDTs until a free one is found
+		uint32_t i;
+		for (i = 0; i < NumBuffers; i++) {
+			if (BufferDescriptors[i].Status == MEMPROG_BUFFER_STATUS_FREE) {
+				BufferDescriptors[i].Status = MEMPROG_BUFFER_STATUS_PENDING;
+				BufferDescriptors[i].Interface = Interface;
+				return static_cast<volatile uint8_t*>(Buffer + (i * BufferSize));
 			}
 		}
 		return nullptr;
 	}
+
+	volatile uint8_t * GetNextFullBuffer() {
+		// Find a full buffer assigned to this interface. If there are multiple, return the one with the lowest address
+		uint32_t Address = 0xFFFFFFFF;
+		uint32_t i;
+		for (i = 0; i < NumBuffers; i++) {
+			if (BufferDescriptors[i].Status == MEMPROG_BUFFER_STATUS_FULL && BufferDescriptors[i].Address < Address) {
+				Address = BufferDescriptors[i].Address;
+			}
+		}
+		return Address == 0xFFFFFFFF ? nullptr : reinterpret_cast<volatile uint8_t*>(Address);
+	}
+
+	void FillBuffer(const volatile uint8_t * const Address) {
+		BufferDescriptors[BufferIndex(Address)].Status = MEMPROG_BUFFER_STATUS_FULL;
+	}
+
+	void ReleaseBuffer(const volatile uint8_t * const Address) {
+		BufferDescriptors[BufferIndex(Address)].Status = MEMPROG_BUFFER_STATUS_FREE;
+	}
+
+protected:
+	MEMPROG_PARAM LocalParam;
+//	MEMPROG_STATUS Status;
+//	uint32_t Code;
+
+	using CMD_FUNC = void (MemProg::*)();
+
+	virtual CMD_FUNC GetHandler(MEMPROG_CMD Command) {
+		return nullptr;
+	}
+
+
+
+private:
+	static MemProg ** Interfaces;
+
+//	inline bool _CMD_DEFAULT() {
+//		return true;
+//	}
+
+	virtual void CMD_QUERY_CAP() {
+		LocalParam.P1 = (uint32_t)Buffer;
+		LocalParam.P2 = BufferSize;
+		LocalParam.P3 = NumBuffers;
+		LocalParam.Status = MEMPROG_STATUS_OK;
+	}
+//	virtual bool CMD_CONTINUE_QUERY_CAP() { return _CMD_DEFAULT(); }// __attribute__ ((weak, alias ("_ZN7MemProg12_CMD_DEFAULTEv")));
+//	virtual bool CMD_START_MASS_ERASE() { return _CMD_DEFAULT(); }// __attribute__ ((weak, alias ("_ZN7MemProg12_CMD_DEFAULTEv")));
+//	virtual bool CMD_CONTINUE_MASS_ERASE() { return _CMD_DEFAULT(); }// __attribute__ ((weak, alias ("_ZN7MemProg12_CMD_DEFAULTEv")));
+//	virtual bool CMD_START_PROG_INIT() { return _CMD_DEFAULT(); }// __attribute__ ((weak, alias ("_ZN7MemProg12_CMD_DEFAULTEv")));
+//	virtual bool CMD_CONTINUE_PROG_INIT() { return _CMD_DEFAULT(); }// __attribute__ ((weak, alias ("_ZN7MemProg12_CMD_DEFAULTEv")));
+//	virtual bool CMD_START_PROG() { return _CMD_DEFAULT(); }// __attribute__ ((weak, alias ("_ZN7MemProg12_CMD_DEFAULTEv")));
+//	virtual bool CMD_CONTINUE_PROG() { return _CMD_DEFAULT(); }// __attribute__ ((weak, alias ("_ZN7MemProg12_CMD_DEFAULTEv")));
+//	virtual bool CMD_START_PROG_FINI() { return _CMD_DEFAULT(); }// __attribute__ ((weak, alias ("_ZN7MemProg12_CMD_DEFAULTEv")));
+//	virtual bool CMD_CONTINUE_PROG_FINI() { return _CMD_DEFAULT(); }// __attribute__ ((weak, alias ("_ZN7MemProg12_CMD_DEFAULTEv")));
+
+//	struct CMD_INFO {
+//		MEMPROG_CMD Command;
+//		CMD_FUNC Func;
+//	};
+
+//	// NOTE: Make sure to update memprog.h:MEMPROG_CMD if changing these
+//	static constexpr CMD_INFO COMMAND_MAP[]{
+//			{MEMPROG_CMD_QUERY_CAP,  &MemProg::CMD_QUERY_CAP},
+//			{MEMPROG_CMD_MASS_ERASE, &MemProg::CMD_MASS_ERASE},
+//			{MEMPROG_CMD_PROG_INIT,  &MemProg::CMD_PROG_INIT},
+//			{MEMPROG_CMD_PROG,       &MemProg::CMD_PROG},
+//			{MEMPROG_CMD_PROG_FINI,  &MemProg::CMD_PROG_FINI},
+//	};
+
+	const uint32_t Interface;
+	volatile MEMPROG_PARAM & Param;
+	volatile MEMPROG_BDT * const BufferDescriptors;
+	volatile uint8_t * const Buffer;
+	const uint32_t BufferSize;
+	const uint32_t NumBuffers;
+
+//	const CMD_INFO * CurrentCommandInfo;
+	CMD_FUNC CurrentHandler;
+	bool Active;
+
+	CMD_FUNC BaseGetHandler(MEMPROG_CMD Command) {
+		switch (Command) {
+			case MEMPROG_CMD_QUERY_CAP:
+				return &MemProg::CMD_QUERY_CAP;
+
+			default:
+				return GetHandler(Command);
+		}
+	}
+
+	void Run() {
+		if (Param.Token == MEMPROG_TOKEN_HOST) {
+			return;
+		}
+		if (Param.Interface != Interface) {
+			return;
+		}
+
+		if (!Active) {
+			if (Param.Status == MEMPROG_STATUS_BUSY) {
+				Active = true;
+//				LocalParam = MEMPROG_PARAM {};
+				LocalParam.Status = MEMPROG_STATUS_BUSY;
+				// Host wants to start a command
+				// Check if a handler for this command exists
+				if (!(CurrentHandler = BaseGetHandler(Param.Command))) {
+					LocalParam.Status = MEMPROG_STATUS_ERR_PARAM;
+				}
+			}
+		}
+
+		if (Active) {
+			if (LocalParam.Status == MEMPROG_STATUS_BUSY) {
+				(this->*CurrentHandler)();
+			}
+
+			if (LocalParam.Status != MEMPROG_STATUS_BUSY) {
+				Active = false;
+				CurrentHandler = nullptr;
+
+				MEMPROG_STATUS RealStatus = LocalParam.Status;
+				LocalParam.Status = MEMPROG_STATUS_BUSY;
+
+				memcpy(Param, LocalParam, sizeof(MEMPROG_PARAM));
+				Param.Status = RealStatus;
+			}
+		}
+	}
+
+//	void Inspect(MEMPROG_CMD & CurrentCommand, MEMPROG_STATUS &CurrentStatus, uint32_t &CurrentCode) {
+//		if (CurrentCommandInfo) {
+//			CurrentCommand = CurrentCommandInfo->Command;
+//		} else {
+//			CurrentCommand = MEMPROG_CMD_NONE;
+//		}
+//
+//		CurrentStatus = Status;
+//		CurrentCode = Code;
+//	}
 };
