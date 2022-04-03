1b8b54ed17ae2d457b670c24a09ba5bd4bd7a879
ichichkine <ichichkine@deltacontrols.com>
2022-04-03 14:52:41 -0700

    wip

--- memprog.h
+++ memprog.h
@@ -2,51 +2,12 @@
 
 #include <stdint.h>
 
-//inline void MEMPROG_SET8(uint8_t * const Buffer, uint8_t Data) {
-//	*Buffer = Data;
-//}
-//inline uint8_t MEMPROG_GET8(const uint8_t * const Buffer) {
-//	return *Buffer;
-//}
-//inline void MEMPROG_SET32(uint8_t * const Buffer, uint8_t Data) {
-//	Buffer[0] = (Data >> 0) & 0xFF;
-//	Buffer[1] = (Data >> 8) & 0xFF;
-//	Buffer[2] = (Data >> 16) & 0xFF;
-//	Buffer[3] = (Data >> 24) & 0xFF;
-//}
-//inline uint32_t MEMPROG_GET32(const uint8_t * const Buffer) {
-//	return
-//			(Buffer[3] << 24) |
-//			(Buffer[2] << 16) |
-//			(Buffer[1] << 8) |
-//			(Buffer[0] << 0);
-//}
-//
-//// Only use these on little-endian targets. Ok to use on the MCU side to save space/time compared to the generic version
-//inline void MEMPROG_SET32_LE(uint8_t * const Buffer, uint8_t Data) {
-//	*((uint32_t*)Buffer) = Data;
-//}
-//inline uint32_t MEMPROG_GET32_LE(const uint8_t * const Buffer) {
-//	return *((uint32_t*)Buffer);
-//}
-
 #define MEMPROG_MAJOR_VERSION  ((uint32_t)2)
 #define MEMPROG_MINOR_VERSION  ((uint32_t)0)
 #define MEMPROG_PATCH_VERSION  ((uint32_t)0)
 #define MEMPROG_VERSION        ((MEMPROG_MAJOR_VERSION << 16) | ((MEMPROG_MINOR_VERSION) << 8) | (MEMPROG_PATCH_VERSION))
 
-
-// NOTE: Make sure to update memprog.py if these flags change
-// TODO move this to another file. Host specific stuff should not be here
-typedef enum {
-	// Execute mass erase every time `program_via_algorithm` is called
-	MEMPROG_OOCD_FLAG_ERASE             = 0x00000001,
-	// Execute default verification algorithm at the end of `program_via_algorithm` (read back memory over SWD)
-	//  This obviously won't work for memory that is not directly accessible over SWD
-	MEMPROG_OOCD_FLAG_VERIFY            = 0x00000002,
-} MEMPROG_OOCD_FLAG;
-
-
+// TODO how to read these from python?
 typedef enum __attribute__((__packed__)) {
 	// Indicates that any interface is free to overwrite the params (assuming they hold the token of course)
 	_MEMPROG_STATUS_IDLE                = 0x00,
@@ -56,6 +17,7 @@ typedef enum __attribute__((__packed__)) {
 	// Set by the target to indicate that a command has been received. This lets the host know that
 	// it can continue and set status back to IDLE
 	_MEMPROG_STATUS_ACK                 = 0x02,
+	// NOTE since only one side will ever see START or ACK, we could use the same value for both
 
 	// Values below 0x40 are reserved. Do not use. Values >= 0x40 are return statuses
 
@@ -68,30 +30,31 @@ typedef enum __attribute__((__packed__)) {
 	MEMPROG_STATUS_ERR_PARAM            = 0x80,
 	MEMPROG_STATUS_ERR_EXECUTION        = 0x81,
 	MEMPROG_STATUS_ERR_TIMEOUT          = 0x82,
-
+	MEMPROG_STATUS_ERR_IMPLEMENTATION   = 0xFE,
 	MEMPROG_STATUS_ERR_OTHER            = 0xFF,
 } MEMPROG_STATUS;
 
+// TODO how to read these from python?
 typedef enum __attribute__((__packed__)) {
 	// IN
 	//  None
 	// OUT
 	//  None
-	MEMPROG_CMD_MASS_ERASE,
+	MEMPROG_CMD_MASS_ERASE              = 0x00,
 
 	// IN
 	//  P1: Start address
 	//  P2: Length
 	// OUT
 	//  None
-	MEMPROG_CMD_ERASE_RANGE,
+	MEMPROG_CMD_ERASE_RANGE             = 0X01,
 
 	// IN
 	//  P1: //Start address (obsoleted by BDTs)
 	//  P2: Total length (to know when all buffers have been received from host)
 	// OUT
 	//  None
-	MEMPROG_CMD_PROG,
+	MEMPROG_CMD_PROG                    = 0X10,
 
 	// IN
 	//  P1: //Start address (obsoleted by BDTs)
@@ -99,7 +62,7 @@ typedef enum __attribute__((__packed__)) {
 	// OUT
 	//  P1: checksum of data
 	// Target should read back after programming and use that to calculate checksum
-	MEMPROG_CMD_PROG_VERIFY,
+	MEMPROG_CMD_PROG_VERIFY             = 0X11,
 
 
 
@@ -122,19 +85,6 @@ typedef enum __attribute__((__packed__)) {
 	// Target is allowed to modify params and BDTs
 	MEMPROG_TOKEN_TARGET                = 0x80,
 } MEMPROG_TOKEN;
-
-//typedef struct {
-//	uint8_t Raw[
-//		1 + // Status
-//		1 + // Token
-//		1 + // Interface
-//		1 + // Command
-//		4 + // Code / Sequence
-//		4 + // Address / P1
-//		4 + // Length / P2
-//		4   // BufferAddress / P3
-//	];
-//} MEMPROG_PARAM;
 
 // FIXME struct packing is compiler defined. This is almost guaranteed to not work correctly on some combination
 //  of target / compiler. Should instead use `uint8_t Params[]` and have e.g. `void SetAddress(uint32_t Address) { Params[4] = Address & 0xFF, ...}`
--- memprog.hpp
+++ memprog.hpp
@@ -6,9 +6,8 @@
 // Base class which defines the constructor and provides stubs for command methods
 class MemProg {
 private:
-	// These must be defined in a source file in the MCU firmware
-
-	// Array of base pointers to the derived MemProg instances
+	// NOTE ============= These must be defined in a source file in the MCU firmware
+	// Array of base pointers to the MemProg subclass instances
 	static MemProg * const Interfaces[];
 	// Address of the parameters structure
 	static volatile MEMPROG_PARAM * const Param;
@@ -16,52 +15,55 @@ private:
 	static volatile MEMPROG_BDT * const BufferDescriptors;
 	// Address of the buffers
 	static volatile uint8_t * const Buffer;
-	// Size of each buffer
+	// Size of each buffer chunk
 	static const uint32_t BufferSize;
-	// Number of buffers
+	// Number of chunks to split the buffer into
+	// With 2 or more chunks, memprog will run command handlers at the same time as receiving data over SWD
 	static const uint32_t NumBuffers;
 
-	// Debug putc function. Set as nullptr to disable debugging
+	// Optional Debug putc function. Set as nullptr to disable debugging
 	static void (* const putc)(uint8_t);
-	// Debug logging verbosity. 0 = highest verbosity
-	static const uint8_t LogLevel;
+	// Optional timekeeping function for debugging. Return millseconds. Set as nullptr if not used
+	static uint32_t (* const time_ms)();
+	// NOTE ========================================================================
+
+	static constexpr uint32_t HANDLER_TIMEOUT_MS = 10;
+	static constexpr uint8_t hex[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
 protected:
-//	static void LogPrints(std::string_view Message, uint8_t Level) {
-//		if (dputc && LogLevel <= Level) {
-//			for (auto c: Message) {
-//				dputc(c);
-//			}
-//		}
-//	}
-	static void lputc(uint8_t c, uint8_t Level=LOG_DEBUG) {
-		if (putc && LogLevel <= Level) {
+	static void lputc(uint8_t c) {
+		if (putc) {
 			putc(c);
 		}
 	}
-	void log(uint8_t c) const {
-		lputc(c);
-		lputc(Interface);
-		lputc(LocalParam.Command);
+	static void lputs(const char* s) {
+		if (putc) {
+			while(*s) {
+				putc(*s);
+				s++;
+			}
+		}
 	}
-//	static void LogDebug(std::string_view Message) { LogPrints(Message, 0); }
-//	static void LogInfo(std::string_view Message) { LogPrints(Message, 10); }
-//	static void LogWarn(std::string_view Message) { LogPrints(Message, 20); }
-//	static void LogError(std::string_view Message) { LogPrints(Message, 30); }
-//	static void LogDebug(uint8_t c) { LogPrintc(c, 0); }
-//	static void LogInfo(uint8_t c) { LogPrintc(c, 10); }
-//	static void LogWarn(uint8_t c) { LogPrintc(c, 20); }
-//	static void LogError(uint8_t c) { LogPrintc(c, 30); }
+	static void lend() {
+		lputc('\n');
+	}
+	static void lputh1(uint8_t v, bool space=true) {
+		lputc(hex[v >> 4]);
+		lputc(hex[v & 0x0F]);
+		if (space) lputc(' ');
+	}
+	static void lputh4(uint32_t v, bool space=true) {
+		lputh1((uint8_t) (v >> 24), false);
+		lputh1((uint8_t) (v >> 16), false);
+		lputh1((uint8_t) (v >> 8), false);
+		lputh1((uint8_t) v, space);
+	}
+	void log(const char *s) const {
+		lputh1(Interface);
+		lputs(s);
+	}
 
 public:
-	static constexpr uint8_t LOG_DEBUG = 0;
-	static constexpr uint8_t LOG_INFO = 0;
-	static constexpr uint8_t LOG_WARN = 0;
-	static constexpr uint8_t LOG_ERROR = 0;
-
-//	MemProg() : LocalParam(), CurrentHandler(nullptr), Active(false), Interface(0)
-//	{
-//	}
 	virtual ~MemProg() = default;
 
 	static void StaticInit() {
@@ -83,13 +85,13 @@ public:
 		Param->Token = MEMPROG_TOKEN_HOST;
 	}
 
-	class ReleaseToken {
-	public:
-		virtual ~ReleaseToken() {
-			// Allow host to process
-			Param->Token = MEMPROG_TOKEN_HOST;
-		}
-	};
+//	class ReleaseToken {
+//	public:
+//		virtual ~ReleaseToken() {
+//			// Allow host to process
+//			Param->Token = MEMPROG_TOKEN_HOST;
+//		}
+//	};
 
 	static void StaticRun() {
 		// TODO select which instance to run based on:
@@ -97,41 +99,93 @@ public:
 		//  - whether it's waiting for a free buffer (must be active for this to be true)
 		//  - which instance ran last time (don't run the same one twice if another command is active/waiting)
 
-		// Currently, we just run them sequentially
-		MemProg * const * ptr;
+		MemProg * inst;
 
-		if (Param->Token != MEMPROG_TOKEN_TARGET) {
-			return;
+		// if we have the token
+		if (Param->Token == MEMPROG_TOKEN_TARGET) {
+			bool released = false;
+
+			// Check if host wants to start a command
+			if (Param->Status == _MEMPROG_STATUS_START) {
+				inst = Interfaces[Param->Interface];
+				// Copy the volatile params to LocalParams
+				inst->Active = true;
+				memcpy(&inst->LocalParam, (const void *)Param, sizeof(LocalParam));
+
+				inst->log("start "); lputh1(inst->LocalParam.Command); lend();
+
+				// Acknowledge the command by changing status to IDLE and passing token back after copying Params
+				Param->Status = _MEMPROG_STATUS_ACK;
+				Param->Token = MEMPROG_TOKEN_HOST;
+				released = true;
+
+				// Check if a handler for this command exists
+				if (!(inst->CurrentHandler = inst->BaseGetHandler(Param->Command))) {
+					inst->LocalParam.Status = MEMPROG_STATUS_ERR_IMPLEMENTATION;
+				}
+			} else if (Param->Status == _MEMPROG_STATUS_IDLE) {
+				// If not, check if anything needs to be returned
+				uint8_t i = 0;
+				for (; (inst = Interfaces[i]); i++) {
+					// if status has been set, the command has finished; notify the host by modifying Param.Status
+					if (inst->Active && inst->LocalParam.Status >= MEMPROG_STATUS_OK) {
+						inst->log("return "); lputh1(inst->LocalParam.Command); lend();
+
+						inst->Active = false;
+						inst->CurrentHandler = nullptr;
+
+//						LocalParam.Token = MEMPROG_TOKEN_TARGET; This is necessarily already TOKEN_TARGET based on the first if statement in this function
+
+						memcpy((void *)Param, &inst->LocalParam, sizeof(MEMPROG_PARAM));
+						// Params.Token must be changed after all the other params. It indicates to the host
+						// that all other params are valid to read
+						Param->Token = MEMPROG_TOKEN_HOST;
+						released = true;
+						break;
+					}
+				}
+			} else {
+				lputs("BAD STATUS "); lputh1(Param->Status); lend();
+			}
+
+			if (!released) {
+				Param->Token = MEMPROG_TOKEN_HOST;
+			}
 		}
 
-		{
-			ReleaseToken s;
-			ptr = Interfaces;
-
-			for (; *ptr; ptr++) {
-				// find the index of the current interface
-				if (*ptr == CurrentInterface) {
-					// and choose the next one
-					CurrentInterface = *(ptr + 1);
-					break;
-				}
+		// then run any active handlers
+		uint32_t end_time = time_ms ? (time_ms() + 10) : 0;
+		uint8_t i = 0;
+		for (; (inst = Interfaces[i]); i++) {
+			if ( ! (inst->Active && inst->LocalParam.Status < MEMPROG_STATUS_OK)) {
+				continue;
 			}
 
-			// if we reached the end of the list, go back to the start
-			if (!CurrentInterface) {
-				CurrentInterface = Interfaces[0];
+			// if status hasn't been set yet, keep running the command
+			inst->log("run "); lputh1(inst->LocalParam.Command); lend();
+			(inst->*inst->CurrentHandler)();
+
+			// Status will be START the first time this is called. Handlers can use this fact to reset their state
+			// After the first run, change Status to something else, unless it has already changed to a valid return value
+			if (inst->LocalParam.Status < MEMPROG_STATUS_OK) {
+				inst->LocalParam.Status = _MEMPROG_STATUS_IDLE;
+			} else {
+				inst->log("finish "); lputh1(inst->LocalParam.Command); lend();
 			}
 
-			// TODO if nothing ran, try another interface (until all are tried, then return to shell)
-			CurrentInterface->Run();
+			if (time_ms && (time_ms() > end_time)) {
+				uint32_t overrun = time_ms() - end_time;
+				lputs("LOOP OVERRUN "); lputh4(overrun); lend();
+				break;
+			}
 		}
 	}
 
 private:
-	static inline uint8_t BufferIndex(const volatile MEMPROG_BDT * const bdt) {
-		return bdt - BufferDescriptors;
-	}
-	inline void DEFAULT_HANDLER() { LocalParam.Status = MEMPROG_STATUS_ERR_OTHER; }
+//	static inline uint8_t BufferIndex(const volatile MEMPROG_BDT * const bdt) {
+//		return bdt - BufferDescriptors;
+//	}
+	inline void DEFAULT_HANDLER() { LocalParam.Status = MEMPROG_STATUS_ERR_IMPLEMENTATION; }
 
 protected:
 	MEMPROG_PARAM LocalParam;
@@ -150,46 +204,71 @@ protected:
 	virtual inline void CMD_PROG() { DEFAULT_HANDLER(); }
 	virtual inline void CMD_PROG_VERIFY() { DEFAULT_HANDLER(); }
 
-	static uint8_t * GetBufferAddress(const MEMPROG_BDT * const bdt) {
-		return const_cast<uint8_t *>(Buffer + BufferIndex(bdt) * BufferSize);
+	static uint8_t * GetBufferAddress(uint8_t BufferIndex) {
+		return const_cast<uint8_t *>(Buffer + BufferIndex * BufferSize);
 	}
 
-	MEMPROG_BDT * AcquireBuffer() const {
+
+	// NOTE these four BDT functions may be called while the token is not held
+	//  Should be OK because the host and target access to BDTs will not overlap in a destructive manner
+	//  During a host write command:
+	//   - host will only write to BDT once Status == FREE
+	//   - target will only read from BDT once Status == FULL
+	//   - target will only write to BDT to change Status back to FREE or PENDING
+	//  During a host read command:
+	//   - target will only write from BDT once Status == FREE
+	//   - host will only read from BDT once Status == FULL
+	//   - host will only write to BDT to change Status back to FREE or PENDING
+
+	void AcquireBuffer(int *BufferIndex) const {
 		// Loop through BDTs until a free one is found
 		uint8_t i;
+
+		*BufferIndex = -1;
 		for (i = 0; i < NumBuffers; i++) {
 			volatile MEMPROG_BDT &bdt = BufferDescriptors[i];
 			if (bdt.Status == MEMPROG_BUFFER_STATUS_FREE) {
 				bdt.Status = MEMPROG_BUFFER_STATUS_PENDING;
 				bdt.Interface = Interface;
-				return const_cast<MEMPROG_BDT *>(&bdt);
+				log("acquire "); lputh1(i); lend();
+				*BufferIndex = i;
 			}
 		}
-		return nullptr;
+		log("acquire -\n");
 	}
 
-	MEMPROG_BDT * GetNextFullBuffer() const {
+	void GetNextFullBuffer(int * BufferIndex, uint32_t *Address, uint32_t *Length) const {
 		// Find a full buffer assigned to this interface. If there are multiple, return the one with the lowest address
-		uint32_t Address = 0xFFFFFFFF;
-		volatile MEMPROG_BDT * bestbdt = nullptr;
+		*Address = 0xFFFFFFFF;
+		*BufferIndex = -1;
 
 		uint8_t i;
 		for (i = 0; i < NumBuffers; i++) {
 			volatile MEMPROG_BDT &bdt = BufferDescriptors[i];
-			if (bdt.Interface == Interface && bdt.Status == MEMPROG_BUFFER_STATUS_FULL && bdt.Address < Address) {
-				Address = bdt.Address;
-				bestbdt = &bdt;
+			if (bdt.Interface == Interface && bdt.Status == MEMPROG_BUFFER_STATUS_FULL && bdt.Address < *Address) {
+				*Address = bdt.Address;
+				*Length = bdt.Length;
+				*BufferIndex = i;
 			}
 		}
-		return const_cast<MEMPROG_BDT *>(bestbdt);
+		if (*BufferIndex >= 0) {
+			log("get "); lputh1(*BufferIndex); lputh4(*Address); lputh4(*Length); lend();
+		} else {
+			log("get -\n");
+		}
 	}
 
-	static void FillBuffer(const MEMPROG_BDT * const bdt) {
-		BufferDescriptors[BufferIndex(bdt)].Status = MEMPROG_BUFFER_STATUS_FULL;
+	void FillBuffer(uint8_t BufferIndex, uint32_t Address, uint32_t Length) const {
+		log("fill "); lputh1(BufferIndex); lend();
+		BufferDescriptors[BufferIndex].Interface = Interface;
+		BufferDescriptors[BufferIndex].Address = Address;
+		BufferDescriptors[BufferIndex].Length = Length;
+		BufferDescriptors[BufferIndex].Status = MEMPROG_BUFFER_STATUS_FULL;
 	}
 
-	static void ReleaseBuffer(const MEMPROG_BDT * const bdt) {
-		BufferDescriptors[BufferIndex(bdt)].Status = MEMPROG_BUFFER_STATUS_FREE;
+	void ReleaseBuffer(uint8_t BufferIndex) const {
+		log("release "); lputh1(BufferIndex); lend();
+		BufferDescriptors[BufferIndex].Status = MEMPROG_BUFFER_STATUS_FREE;
 	}
 
 private:
@@ -226,71 +305,6 @@ private:
 			// Interface-specific commands
 			default:
 				return GetHandler(Command);
-		}
-	}
-
-	// Return true if a command was run
-	void Run() {
-		if (!Active) {
-			// Interface only needs to be checked to start a new command
-			//  If a command is already active, it doesn't read from Params, so we can just run it
-			if (Param->Interface != Interface) {
-				return;
-			}
-
-			// Check if host wants to start a command
-			if (Param->Status == _MEMPROG_STATUS_START) {
-				// Copy the volatile params to LocalParams
-				Active = true;
-				memcpy(&LocalParam, (const void *)Param, sizeof(LocalParam));
-
-				log(0x00);
-
-				// Acknowledge the command by changing status to IDLE and passing token back after copying Params
-				Param->Status = _MEMPROG_STATUS_ACK;
-
-				// Check if a handler for this command exists
-				if (!(CurrentHandler = BaseGetHandler(Param->Command))) {
-					LocalParam.Status = MEMPROG_STATUS_ERR_PARAM;
-				}
-			}
-		}
-
-		if (Active) {
-			// if status hasn't been set yet, keep running the command
-			if (LocalParam.Status < MEMPROG_STATUS_OK) {
-				// Status will be START the first time this is called. Handlers can use this fact to reset their state
-				log(0x01);
-
-				(this->*CurrentHandler)();
-				// After the first run, change Status to something else, unless it has already changed to a valid return value
-				if (LocalParam.Status < MEMPROG_STATUS_OK) {
-					LocalParam.Status = _MEMPROG_STATUS_IDLE;
-				} else {
-					log(0x02);
-				}
-			}
-
-			// if status has been set, the command has finished; notify the host by modifying Param.Status
-			if (LocalParam.Status >= MEMPROG_STATUS_OK) {
-				if (Param->Status != _MEMPROG_STATUS_IDLE) {
-					// We can only write to Param if Status == IDLE, otherwise we would be overwriting a pending command
-					// or returned data from another interface. In this case just return and try again next time
-					return;
-				}
-
-				log(0x10);
-
-				Active = false;
-				CurrentHandler = nullptr;
-
-//				LocalParam.Token = MEMPROG_TOKEN_TARGET; This is necessarily already TOKEN_TARGET based on the first if statement in this function
-
-				memcpy((void *)Param, &LocalParam, sizeof(MEMPROG_PARAM));
-				// Params.Token must be changed after all the other params. It indicates to the host
-				// that all other params are valid to read
-				Param->Token = MEMPROG_TOKEN_HOST;
-			}
 		}
 	}
 };
